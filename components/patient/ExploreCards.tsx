4c5a4b4a679f146180e3c8ee2a7712c90cb99609
Hazem Samir <hazemyugi010@gmail.com>
2025-01-06 14:56:53 +0200

    Remove Text

--- components/patient/ExploreCards.tsx
+++ components/patient/ExploreCards.tsx
@@ -20,7 +20,7 @@ export default function ExploreCards() {
           <h2 className="text-2xl font-semibold mb-2">Explore Pharmacies</h2>
           <p className="text-gray-600 mb-4">Find medications and health products</p>
           <Link href="/patient/pharmacies" className="
-          inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-4 py-2 bg-teal-400 text-primary-foreground hover:bg-teal-400/90">Go to Go to Pharmacies</Link>
+          inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-4 py-2 bg-teal-400 text-primary-foreground hover:bg-teal-400/90">Go to Pharmacies</Link>
         </CardContent>
       </Card>
       <Card className="overflow-hidden bg-card">
